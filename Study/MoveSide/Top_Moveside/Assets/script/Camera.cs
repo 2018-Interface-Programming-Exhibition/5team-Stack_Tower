c28c35f5e1b06c65b95182ae2f88ea7477f6997e
DESKTOP-762426N\jekke <jekker77@gmail.com>
2018-10-01 00:39:37 +0900

    코딩했는데 막혀버림ㅁ뮤ㅠㅠㅠㅠ

---
+++ Study/MoveSide/Top_Moveside/Assets/script/Camera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public float speed = 1f;
+    public GameObject Cameraposition;
+    public GameObject Main;
+    Transform C_P;
+    Transform M_P;
+    FirstCamera firstcamera = GameObject.Find("First Position Button").GetComponent<FirstCamera>();
+    // Use this for initialization
+    void Start ()
+    {
+        C_P = Cameraposition.transform;
+        M_P = Cameraposition.transform;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        if (firstcamera.Ca_Sw == false)
+        {
+            Vector3 moveCamera = new Vector3(0, speed);
+            transform.position += moveCamera * 1f * Time.deltaTime;
+            transform.position = new Vector3(C_P.position.x, C_P.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Study/MoveSide/Top_Moveside/Assets/script/FirstCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstCamera : MonoBehaviour
+{
+    public bool Ca_Sw = false;
+    private void OnMouseUp()
+    {
+        Ca_Sw = true;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
