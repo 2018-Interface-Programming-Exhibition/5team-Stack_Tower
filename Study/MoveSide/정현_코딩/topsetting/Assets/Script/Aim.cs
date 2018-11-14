17281fdf5dd1bf1fc31072b9233e00e034f0b78a
이동엽 <jekker77@gmail.com>
2018-11-14 20:55:51 +0900

    정현 & 동엽 유니티 합침

---
+++ Study/MoveSide/정현_코딩/topsetting/Assets/Script/Aim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+    float Aim_x, AimPoint_x;
+    public GameObject AimPoint;
+        // Use this for initialization
+        void Start ()
+        {
+        AimPoint = GameObject.Find("AimPoint");
+        Aim_x = transform.localPosition.x;//Aim의 x좌표
+        AimPoint_x = AimPoint.transform.position.x;//AimPoint의 x좌표
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Aim_x = transform.localPosition.x;//Aim의 x좌표를 매번 저장해준다
+        AimPoint_x = AimPoint.transform.position.x;//AimPoint의 x좌표를 매번 저장해준다
+        // Debug.Log(Aim_x);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if ((AimPoint_x-2.3f)<=Aim_x&&Aim_x<= (AimPoint_x + 2.3f))//Aim이 AimPoint의 안에 있으면 새를 생성하고 AimPoint의 x좌표를 바꿔준다
+            {
+                Debug.Log("안에 있음");
+                GameObject.Find("GameManager").SendMessage("CreateBird");
+                GameObject.Find("AimPoint").SendMessage("X_PositionChange");
+            }
+            
+        }
+
+    }
+}
+
+
+
---
+++ Study/MoveSide/정현_코딩/topsetting/Assets/Script/CubeMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove: MonoBehaviour {
+    float delta = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        float newXPosition = transform.position.x + delta;
+        transform.position = new Vector3(newXPosition, transform.position.y, 0);
+
+        if ( newXPosition < -4.0f )
+        {
+     
+            delta *=(-1);
+        }
+        else if( newXPosition > 4.0f)
+        {
+            
+            delta *= (-1);
+        }
+    }
+}
