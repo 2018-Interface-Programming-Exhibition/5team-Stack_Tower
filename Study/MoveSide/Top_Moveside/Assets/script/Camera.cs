ed9c97a1e17be06b0a6b03d9159045156280d508
DESKTOP-762426N\jekke <jekker77@gmail.com>
2018-11-14 20:58:38 +0900

    이전파일들

--- Study/MoveSide/Top_Moveside/Assets/script/Camera.cs
+++ Study/MoveSide/Top_Moveside/Assets/script/Camera.cs
@@ -6,6 +6,7 @@ public class Camera : MonoBehaviour {
     public float speed = 1f;
     public GameObject Cameraposition;
     public GameObject Main;
+    public static float limit;
     Transform C_P;
     Transform M_P;
     FirstCamera firstcamera = GameObject.Find("First_Position_Button").GetComponent<FirstCamera>();
@@ -13,22 +14,31 @@ public class Camera : MonoBehaviour {
     void Start ()
     {
         C_P = Cameraposition.transform;
-        M_P = Cameraposition.transform;
+        M_P = Main.transform;
     }
-	
-	// Update is called once per frame
-	void LateUpdate ()
+    void Update()
     {
-        if (FirstCamera.Ca_Sw == false)
+        limit = (float)C_P.position.y - M_P.position.y;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (limit < 5.0)
         {
+            if (FirstCamera.Ca_Sw == false)
+            {
 
-            transform.position = new Vector3(C_P.position.x, C_P.position.y, transform.position.z);
+                transform.position = new Vector3(C_P.position.x, C_P.position.y, transform.position.z);
+            }
+            else
+            {
+                transform.position = new Vector3(M_P.position.x, M_P.position.y, -10);
+            }
         }
         else
         {
-            transform.position = new Vector3(0, 0,-10);
-        }
 
+        }
     }
     void OnMouseUp()
     {
